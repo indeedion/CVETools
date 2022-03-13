0a516b6d419547ffbe52f86fcc45df16de854f47
indeedion <mengus00@gmail.com>
2022-03-13 23:52:42 +0100

    wrote first working version

--- CVESort.sh
+++ CVESort.sh
@@ -1,47 +1,76 @@
 #!/bin/bash
 
-# Print usage message
-function printUsage () {
-	echo "USAGE: ./CVEScore.sh <Path/to/csv>" 
-}
+# Purpose: Add cvss score to CISA exploited vulnerability catalog
+# Author: Indeedion
+# Source: https://github.com/indeedion/CVESort
+# Contact: mengus00@gmail.com
+# Usage: ./CVESort.sh, takes no arguments
 
-# Check for correct number of arguments
-if [ "$#" -ne 1 ]; then
-	    echo "Illegal number of parameters"
-	    printUsage
-	    exit 1
-fi
+# print welcome
+echo "CVESort 1.0, downloads CISA exploited vulnerabilities catalog and adds cvss scores to file for
+easier sorting and overview"
 
 # Gets the corresponding score from nist website
 function getScore () {
 	SCORE=$(curl -s https://nvd.nist.gov/vuln/detail/$1 | grep "Base Score" | cut -d "/" -f 3 | grep cvssv3 | cut -d ";" -f 6 | cut -d " " -f 1)
 }
 
-# Patch to csv
-CSV_PATH="$1"
+# Path to csv
+CSV_PATH="known_exploited_vulnerabilities.csv"
 
-# Parse csv to array
+# Remove old CSV and download new one
+if test -f "$CSV_PATH"; then
+    rm -f $CSV_PATH
+fi
+echo "Downloading catalog from CISA.."
+wget https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv
+
+#Check that download went well
+if ! test -f "$CSV_PATH"; then
+    echo "CSV file not found!"
+    exit 1
+fi
+echo "Download OK"
+
+# Parse CVE's to array
 CVE=()
-CVE=$(cat $1 | cut -d "," -f 1)
+CVE=$(cat $CSV_PATH | cut -d "," -f 1)
 
-# Delete old outputfile if present
-if test -f "$PWD/CVEScoreOut.csv"; then
-	rm -f CVEScoreOut.csv
+# Delete old temporary outputfile if present
+if test -f "tmpout"; then
+    rm -f tmpout
 fi
 
-# Get scores and write new csv file
-for i in ${CVE[@]}; do
-	getScore $i
-	
+echo "Getting cvss and building new file.."
+# loop through csv line by line, get cvss score from nist and create new file
+c=0
+while read i; do
+    if [[ $c == 0 ]]; then	
+	line="$i"
+	first=$(echo $line | cut -d "," -f 1)
+	rest=$(echo $line | cut -d "," -f 2,3,4,5,6,7,8)
+	result="$cve,"CVSS3",$rest"
+	echo $result >> tmpout
+    else
+	line="$i"
+	cve=$(echo $line | cut -d "," -f 1)
+	rest=$(echo $line | cut -d "," -f 2,3,4,5,6,7,8)
+	getScore $cve
 	size=${#SCORE}
 	if [[ $size < 1 ]]; then
 		SCORE="NO cvssv3 score available yet"
 	else
 		SCORE=$(echo $SCORE | tr -s '\n' ' ' | cut -d " " -f 1) 
-		SCORE="$SCORE"
 	fi   
+	result="$cve,$SCORE,$rest"
+	echo $result
+	echo $result >> tmpout
+    fi
+    (( ++c ))
+done < $CSV_PATH
 
-	#echo "Checking $1: $SCORE"	
-	echo "$i;$SCORE" >> CVEScoreOut.csv
-done
+#Replace old file with new
+rm -f $CSV_PATH && mv tmpout "$CSV_PATH"
+
+echo "Done!"
 
